62695137eae324acd0f4c4177603d2459942050b
fadzrinmadu <aan.fadzrin@gmail.com>
2020-07-15 00:47:20 +0700

    refactor code

--- js/app.js
+++ js/app.js
@@ -1,7 +1,10 @@
 $(document).ready(function() {
-  $('.owl-carousel').owlCarousel({
+  const owl = $('.owl-carousel');
+
+  owl.owlCarousel({
     loop: true,
     margin: 20,
+    nav: true,
     responsive: {
       0: {
         items: 1
@@ -11,5 +14,19 @@ $(document).ready(function() {
         autoWidth: true,
       }
     }
+  });
+
+  $('.slider-control-next').click(function(e) {
+    e.preventDefault();
+    owl.trigger('next.owl.carousel');
+  });
+
+  $('.slider-control-prev').click(function(e) {
+    e.preventDefault();
+    owl.trigger('prev.owl.carousel');
+  });
+
+  $('.slider-dot').click(function () {
+    owl.trigger('to.owl.carousel', [$(this).index(), 300]);
   });
 });
